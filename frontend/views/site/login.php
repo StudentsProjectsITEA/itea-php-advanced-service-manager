336e316d7a95ecdb7df5c55591d9c810bb2bd100
Eugene Bilan <eugene.bilan@gmail.com>
2020-03-22 14:43:56 +0200

    merged conflicts frontend/views/site/login.php

--- frontend/views/site/login.php
+++ frontend/views/site/login.php
@@ -2,12 +2,7 @@
 
 /* @var $this yii\web\View */
 /* @var $form yii\bootstrap\ActiveForm */
-<<<<<<< HEAD
-
 /* @var $model \common\models\LoginForm */
-=======
-/* @var $model \frontend\models\LoginForm */
->>>>>>> d037d438680c18810dd82f6cce56e68dd131ecf7
 
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
